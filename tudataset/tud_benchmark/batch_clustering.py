f2ef42703a143cbc07daa171152ba1211195c062
Benedikt Grammer <benedikt.grammer@gmx.at>
2022-11-15 22:41:33 +0100

    updated notebook

--- tudataset/tud_benchmark/batch_clustering.py
+++ tudataset/tud_benchmark/batch_clustering.py
@@ -15,7 +15,7 @@ def nrkmeans_batch(data, labels, clusters = 20):
         nrkm.fit(gram, best_of_n_rounds=10)
         nmis[new_name] = normalized_mutual_info_score(nrkm.labels[0],labels) 
         y_pred[new_name] = nrkm.labels[0] 
-    return nmis,y_pred
+    return nmis,y_preds
 
 def agglomerative_batch(data,labels,clusters = 20,linkage="complete"):
     #‘ward’, ‘complete’, ‘average’, ‘single’
@@ -28,19 +28,19 @@ def agglomerative_batch(data,labels,clusters = 20,linkage="complete"):
         agglm.fit(gram)
         nmis[new_name] = normalized_mutual_info_score(agglm.labels_,labels)
         y_pred[new_name] = agglm.labels_
-    return nmis,y_pred
+    return nmis,y_preds
 
-def spectral_clustering_batch(data,labels,clusters = 20):
+def spectral_clustering_batch(data,labels,assign_labels = "cluster_qr", affinity = 'precomputed', clusters = 20):
     nmis = {}
     y_preds = {}
 
     for name,gram in data.items():
         new_name = name+"_spectral"
-        sc = sklearn.cluster.SpectralClustering(n_clusters=clusters)
+        sc = sklearn.cluster.SpectralClustering(affinity = affinity, assign_labels = assign_labels, n_clusters=clusters)
         sc.fit(gram)
         nmis[new_name] = normalized_mutual_info_score(sc.labels_,labels)
         y_preds[new_name] = sc.labels_
-    return nmis,y_pred
+    return nmis,y_preds
 
 def dbscan_batch(data,labels,eps,min_samples):
     nmis = {}
@@ -51,8 +51,31 @@ def dbscan_batch(data,labels,eps,min_samples):
         dbscan.fit(gram)
         nmis[new_name] = normalized_mutual_info_score(dbscan.labels_,labels)
         y_preds[new_name] = dbscan.labels_
-    return nmis,y_pred
-    
+    return nmis,y_preds
+
+def birch_batch(data,labels,threshold = 0.5, branching_factor = 50, clusters = 20):
+    nmis = {}
+    y_preds = {}
+    for name,gram in data.items():
+        new_name = name + "_birch"
+        birch = sklearn.cluster.Birch(threshold = threshold,
+                                      branching_factor = branching_factor, n_clusters=clusters)
+        y_pred = birch.fit_predict(gram)
+        nmis[new_name] = normalized_mutual_info_score(labels,y_pred)
+        y_preds[new_name] = y_pred
+    return nmis,y_preds
+
+def affinity_batch(data,labels, preference = None):
+    nmis = {}
+    y_preds = {}
+    for name,gram in data.items():
+        new_name = name + "_birch"
+        birch = sklearn.cluster.AffinityPropagation(preference = preference)
+        y_pred = birch.fit_predict(gram)
+        nmis[new_name] = normalized_mutual_info_score(labels,y_pred)
+        y_preds[new_name] = y_pred
+    return nmis,y_preds
+
 def create_img_index_by_class(classes):
     id2img = pd.read_csv(os.path.join(os.getcwd(),"datasets","MSRC_21","MSRC_21","raw","MSRC_21_map_id2im.csv"), sep='\t',header=None)
     file_names = np.array(id2img[1])
